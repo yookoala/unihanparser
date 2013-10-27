8969470c55990ccd736c3785b5ead971f0398590
Koala Yeung <koalay@gmail.com>
2013-10-28 02:48:10 +0800

    Enhance build script
    1. Added better message
    2. Removes previous database file automatically
    3. Added the parseUnihan command to the end of build

--- build.sh
+++ build.sh
@@ -2,7 +2,52 @@
 
 BASE_DIR=$(dirname "$(readlink -f $0)")
 SRC_DIR="$BASE_DIR/src"
+DB_PATH="$BASE_DIR/data/unihan.db"
 
+# check dependencies and try to fix
+echo -ne "Check dependencies ... "
+go list github.com/mattn/go-sqlite3 2>&1 1>/dev/null
+if [ $? -ne 0 ]; then
+  echo -ne "install now ... "
+  go get github.com/mattn/go-sqlite3
+  if [ $? -ne 0 ]; then
+    echo "Failed"
+    echo "Cannot install package github.com/mattn/go-sqlite3"
+    exit 1
+  fi
+fi
+echo "Success"
+
+# remove previously built database file
+if [ -f "$DB_PATH" ]; then
+  echo -ne "Remove old database ... "
+  rm "$DB_PATH"
+  echo "Success"
+fi
+
+# build the parser tool
+echo -ne "building the parser ... "
 cd "$SRC_DIR"
 go fmt && go build -o ../parseUnihan
-exit $?
+RET=$?
+if [ $RET -eq 0 ]; then
+  echo "Success"
+else
+  echo "Failed"
+  exit $RET
+fi
+
+# build the database
+echo -ne "build the database ... "
+cd ..
+./parseUnihan
+RET=$?
+if [ $RET -eq 0 ]; then
+  echo "Success"
+else
+  echo "Failed"
+  exit $RET
+fi
+
+exit $RET
+
