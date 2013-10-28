9d4a41cc06a1f3107ffcddbd21f982f305517d93
Koala Yeung <koalay@gmail.com>
2013-10-29 01:50:06 +0800

    Migrated functions to helpers.go and etc
    1. Migrated functions to helpers.go
    2. Added hexToString helper function

---
+++ src/helpers.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Read a whole file into the memory and store it as array of lines
+func readLines(path string) (lines []string, err error) {
+	var (
+		file   *os.File
+		part   []byte
+		prefix bool
+	)
+	if file, err = os.Open(path); err != nil {
+		return
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	buffer := bytes.NewBuffer(make([]byte, 0))
+	for {
+		if part, prefix, err = reader.ReadLine(); err != nil {
+			break
+		}
+		if len(part) > 0 && part[0] != '#' {
+			buffer.Write(part)
+			if !prefix {
+				lines = append(lines, buffer.String())
+				buffer.Reset()
+			}
+		}
+	}
+	if err == io.EOF {
+		err = nil
+	}
+	return
+}
+
+func parseLine(line string) (item UnihanDataEntry, err error) {
+	item = make([]string, 3)
+	item = strings.Split(line, "\t")
+	return item, nil
+}
+
+func parseUnihanFile(filename string, handlerName string, db *UnihanDB) {
+	tx, err := db.DB.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tx.Commit()
+	lines, err := readLines(filename)
+	if err != nil {
+		panic(err)
+	}
+	for _, line := range lines {
+		item, err := parseLine(line)
+		if err != nil {
+			panic(err)
+		}
+		err = db.Insert(handlerName, tx, item)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func hexToString(h string) (string, error) {
+	return strconv.Unquote(`"\u` + h + `"`)
+}
+
+
+
