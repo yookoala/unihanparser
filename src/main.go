487da4382f454acbbd24d2b65e3bf93d59a1392a
Koala Yeung <koalay@gmail.com>
2013-12-05 00:52:23 +0800

    Accept flag arguments
    Accept 2 flags:
    
     -f  folder that contains unihan files
     -d  output database file full path

--- src/main.go
+++ src/main.go
@@ -1,11 +1,31 @@
 package main
 
-import ()
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	dir := "./data/Unihan-6.3"
+
+	var dir, dbfn string
+
+	flag.StringVar(&dir, "f", "./data/Unihan", "Folder containing Unihan database files")
+	flag.StringVar(&dbfn, "d", "./data/unihan.db", "Output database filename")
+	flag.Parse()
+
+	// check if Unihan database folder is a folder
+	finfo, err := os.Stat(dir)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	} else if !finfo.IsDir() {
+		fmt.Println("The directory \"" + dir + "\" should be a directory")
+		os.Exit(1)
+	}
+
 	db := UnihanDB{
-		Filename: "./data/unihan.db",
+		Filename: dbfn,
 	}
 
 	db.Register("Variants", VariantsHandler{})
